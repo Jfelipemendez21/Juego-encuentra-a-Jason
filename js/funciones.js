a672216bfe83d250e6f9c29b8bf2dae2d98d1f8e
Jfelipemendez21 <jfelipemendez@ucundinamarca.edu.co>
2023-03-23 19:10:33 -0500

    primera version del juego

---
+++ js/funciones.js
@@ -0,0 +1,37 @@
+let posicionAleatoria = (size) => {
+    return Math.floor(Math.random() * size);
+}
+
+let calcularDistanciaDelPuntero = (evento, posicion) => {
+
+    // OFFSET NOS PERMITE SABER LA UBICACION EN X O EN Y DEL EVENTO EN QUE SE DESENCADENO 
+    let puntoX = evento.offsetX - posicion.x;
+    let puntoY = evento.offsetY - posicion.y;
+
+    // TEOREMA DE PITAGORAS (TRIANGULO RECTANGULO)
+    return Math.sqrt((puntoX * puntoX) + (puntoY * puntoY));
+}
+
+
+let avisoDistancia = (distancia) => {
+    if (distancia < 20) {
+        let bosque = document.querySelector(".bosque");
+        bosque.removeAttribute("src");
+        bosque.setAttribute("src", "imgs/json.jpg");
+        return `
+                <h1 class="muerto">Detras de ti!<span>&#160</span></h1>
+                <audio autoplay="" src="audio/grito.mp3" type="audio/mpeg"></audio> 
+                <audio autoplay="" src="audio/golpe.mp3" type="audio/mpeg"></audio>
+        `
+    } else if (distancia < 60) {
+        return `<h1 class="mensajes">Manten los ojos abiertos, esta muy cerca! <span>&#160</span></h1>`
+    } else if (distancia < 100) {
+        return `<h1 class="mensajes">Se cauteloso, puede que este cerca!<span>&#160</span></h1>`
+    } else if (distancia < 200) {
+        return `<h1 class="mensajes">Continua! no estas tan lejos!<span>&#160</span></h1>`
+    } else if (distancia < 360) {
+        return `<h1 class="mensajes">Aun estas lejos del objetivo!<span>&#160</span></h1>`
+    } else {
+        return `<h1 class="mensajes">Te encuentras demaciado alejado!<span>&#160</span></h1>`
+    }
+}
---
+++ js/main.js
@@ -0,0 +1,23 @@
+const w= 600; 
+const h= 400;
+
+let posicion={
+    x: posicionAleatoria(w),
+    y: posicionAleatoria(h)
+}
+
+let bosque= document.querySelector(".bosque");
+
+bosque.addEventListener("click", (e)=>{
+    e.preventDefault();
+    let distancia = calcularDistanciaDelPuntero(e, posicion);
+    let aviso= avisoDistancia(distancia);
+
+    let Json= document.querySelector(".cercania");
+    Json.innerHTML= aviso; 
+    if(distancia<20){
+        setTimeout(()=>{
+            location.reload();
+        },4000)
+    }
+})
