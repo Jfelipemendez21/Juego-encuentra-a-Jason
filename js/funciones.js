580f19c5a3ebcd9baaf0df8f202a153fa3710ab5
Jfelipemendez21 <jfelipemendez@ucundinamarca.edu.co>
2023-03-23 19:51:00 -0500

    version 1.2 del juego

--- js/funciones.js
+++ js/funciones.js
@@ -19,19 +19,19 @@ let avisoDistancia = (distancia) => {
         bosque.removeAttribute("src");
         bosque.setAttribute("src", "imgs/json.jpg");
         return `
-                <h1 class="muerto">Detras de ti!<span>&#160</span></h1>
+                <h1 class="muerto">Detras de ti!!!<span>&#160</span></h1>
                 <audio autoplay="" src="audio/grito.mp3" type="audio/mpeg"></audio> 
                 <audio autoplay="" src="audio/golpe.mp3" type="audio/mpeg"></audio>
         `
     } else if (distancia < 60) {
-        return `<h1 class="mensajes">Manten los ojos abiertos, esta muy cerca! <span>&#160</span></h1>`
+        return `<h1 class="mensajes muycerca">Manten los ojos abiertos, esta muy cerca! <span>&#160</span></h1>`
     } else if (distancia < 100) {
-        return `<h1 class="mensajes">Se cauteloso, puede que este cerca!<span>&#160</span></h1>`
+        return `<h1 class="mensajes cerca">Se cauteloso, puede que este cerca!<span>&#160</span></h1>`
     } else if (distancia < 200) {
-        return `<h1 class="mensajes">Continua! no estas tan lejos!<span>&#160</span></h1>`
+        return `<h1 class="mensajes mediocerca">Continua! no estas tan lejos!<span>&#160</span></h1>`
     } else if (distancia < 360) {
-        return `<h1 class="mensajes">Aun estas lejos del objetivo!<span>&#160</span></h1>`
+        return `<h1 class="mensajes lejos">Aun estas lejos del objetivo!<span>&#160</span></h1>`
     } else {
-        return `<h1 class="mensajes">Te encuentras demaciado alejado!<span>&#160</span></h1>`
+        return `<h1 class="mensajes muylejos">Te encuentras demaciado alejado!<span>&#160</span></h1>`
     }
 }
